519b49dbf7f26fb36d5643d2e7706a357b26d54a
gamilamhr <gamilans@icloud.com>
2023-11-14 09:56:38 -0800

    printf

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT (0, 0, 0, 0, 0, 0, 0, 0, 0,0)
+
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED  2
+#endif /**MAIN_H**/
